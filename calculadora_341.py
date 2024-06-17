4f0a6398ea3e1d75f5f8b53b85c0a63c050ed037
AntonioGE696 <an.barrientos@duocuc.cl>
2024-06-17 12:41:57 -0400

    Add files via upload
    Otra calculadora

---
+++ calculadora_341.py
@@ -0,0 +1,57 @@
+def suma(a,b):
+    return a+b
+
+def resta(a,b):
+    return a-b
+
+def multi(a,b):
+    return a*b
+
+def divi(a,b):
+    if b == 0:
+        print("Error, no se puede dividir por cero")
+  
+    else:
+        b != 0
+        return a/b
+
+
+while True:
+
+    opc = int(input("1.-Sumar, 2.-Restar, 3.-Multiplicar, 4.-Dividir, 5.-Salir:  "))
+    
+
+
+    if opc == 1:
+        a = int(input("Ingrese primer número: "))
+        b = int(input("Ingrese segundo número: "))
+
+        print(suma(a,b))
+
+    if opc == 2:
+        a = int(input("Ingrese primer número: "))
+        b = int(input("Ingrese segundo número: "))
+
+        print(resta(a,b))
+
+    if opc == 3:
+        a = int(input("Ingrese primer número: "))
+        b = int(input("Ingrese segundo número: "))
+
+        print(multi(a,b))
+
+    if opc == 4:
+        a = int(input("Ingrese primer número: "))
+        b = int(input("Ingrese segundo número: "))
+
+        print(divi(a,b))
+
+    if opc == 5:
+        break
+
+    input("Apriete ENTER para seguir")
+
+
+        
+
+
